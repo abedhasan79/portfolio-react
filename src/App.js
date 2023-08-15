39bb815b4432ad846fd913eb7b7f8b641d8dcca1
Abed Hasan <abedhasan79@gmail.com>
2023-08-15 02:55:25 -0400

    project

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './components/navbar';
 import Home from './pages/home';
 import About from './pages/about';
+import Projects from './pages/projects';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
       <About/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/pages/projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './projects.css'
+function Projects(){
+    return(
+        <div className="projectsMainDiv">
+            asfsdgdg
+        </div>
+    )
+}
+
+export default Projects;
