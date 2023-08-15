52ea8028146501ea3f311569212de7f66e9bb72b
Abed Hasan <abedhasan79@gmail.com>
2023-08-15 12:44:31 -0400

    project container

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,45 @@
 import React from "react";
 import './projects.css'
-function Projects(){
-    return(
+function Projects() {
+    return (
         <div className="projectsMainDiv">
-            asfsdgdg
+            <div>
+                <div className="projectHeadding">
+                    <h2>Projects</h2>
+                </div>
+                <div className="projectIntroDiv">
+                    <p className="projectParagraph">
+                        These projects demonstrate my expertise with practical examples of some of my work, including brief descriptions and links to code repositories and live demos. They showcase my ability to tackle intricate challenges, adapt to various technologies, and efficiently oversee projects.
+                    </p>
+                </div>
+
+                <div className="projectParentDiv">
+                    <div className="project projectOne">
+                        <h4>PoKeDex</h4>
+                        <img src="/images/pokedex.png" alt="pokedex" />
+                        <div className="description">
+                            <h6>pokedex app using HTML CSS JavaScript. Uses data from the poke Api to fetch data.</h6>
+
+                        </div>
+                    </div>
+                    <div className="project projectTwo">
+                    <h4>PoKeDex</h4>
+                        <img src="/images/pokedex.png" alt="pokedex" />
+                        <div className="description">
+                            <h6>pokedex app using HTML CSS JavaScript. Uses data from the poke Api to fetch data.</h6>
+
+                        </div>
+                    </div>
+                    <div className="project projectThree">
+                    <h4>PoKeDex</h4>
+                        <img src="/images/pokedex.png" alt="pokedex" />
+                        <div className="description">
+                            <h6>pokedex app using HTML CSS JavaScript. Uses data from the poke Api to fetch data.</h6>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
