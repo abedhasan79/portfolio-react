4578ebb140dd00d1bdef20eccba368f3ccf4f65d
Abed Hasan <abedhasan79@gmail.com>
2023-08-18 17:23:20 -0400

    title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React, {useEffect} from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import Home from './pages/home';
@@ -6,7 +6,12 @@ import About from './pages/about';
 import Projects from './pages/projects';
 import Contact from './pages/contact';
 
+
 function App() {
+
+  useEffect(()=>{
+    document.title = "My Portfolio"
+  },[])
   return (
     <div className="App">
       <Navbar/>
